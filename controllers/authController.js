b939ff53b16a150be0cb2a842e8466d7f0cb0dba
Chris Mckeown <chris.j.mckeown@hotmail.com>
2021-02-22 09:15:33 +1100

    Auth route and controller added. Other routes commented out for testing using postman.

---
+++ controllers/authController.js
@@ -0,0 +1,77 @@
+const db = require('../models/sql');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+
+// Defining methods for the authController
+module.exports = {
+  // @route   GET api/auth
+  // @desc    Test route
+  // @access  Public
+  getAuthMember: async (req, res) => {
+    try {
+      const member = await db.member.findByPk(req.member.id, {
+        attributes: {
+          exclude: ['password'],
+        },
+      });
+      return res.json(member);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send('Server error');
+    }
+  },
+  // @route   POST api/auth
+  // @desc    Authenticate user and get token
+  // @access  Public
+  signIn: async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let member = await db.member.findOne({
+        where: {
+          email,
+        },
+      });
+
+      if (!member) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, member.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+      const payload = {
+        member: {
+          id: member.id,
+          clientId: member.clientId,
+          role: member.role,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          return res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send('Server error');
+    }
+  },
+};
---
+++ routes/api/auth.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const authController = require('../../controllers/authController');
+const auth = require('../../middleware/auth');
+const { check } = require('express-validator');
+
+// Matches with "/api/clients"
+router.get('/', auth, authController.getAuthMember);
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  authController.signIn
+);
+
+module.exports = router;
