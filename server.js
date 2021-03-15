268750583c80d7c5f3b071cb3e357d99f19c9dfd
Chris Mckeown <chris.j.mckeown@hotmail.com>
2021-03-15 18:50:39 +1100

    updated server route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const apiRoutes = require('./routes/api');
+const path = require('path');
 
 const app = express();
 
@@ -12,10 +13,16 @@ app.use(express.json());
 connectDB.connectSQLDB();
 connectDB.connectMongoDB();
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define routes
 app.use(apiRoutes);
+
+//serve stat assest in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
